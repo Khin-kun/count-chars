c9c8068b8180426e5d8b1080bb6e0787457a8354
Khin-kun <87539286+Khin-kun@users.noreply.github.com>
2023-02-03 19:59:54 +0100

    Add files via upload

---
+++ cout_chars_without_space.py
@@ -0,0 +1,7 @@
+#comta caratteri versione 0.0.1
+def count_chars_without_spaces(input_string):
+    return len(input_string.replace(" ", ""))
+
+input_string = input("Inserisci una stringa: ")
+print("Il numero di caratteri senza spazi è: ", count_chars_without_spaces(input_string))
+input("press enter to exit")
