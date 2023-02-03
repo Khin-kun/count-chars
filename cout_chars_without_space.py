c8f0be2ae926c96938bdad502bdc6b8310d99499
Khin-kun <87539286+Khin-kun@users.noreply.github.com>
2023-02-03 20:04:57 +0100

    Update cout_chars_without_space.py

--- cout_chars_without_space.py
+++ cout_chars_without_space.py
@@ -1,6 +1,6 @@
 def count_chars_without_spaces(input_string):
     return len(input_string.replace(" ", ""))
 
-input_string = input("Inserisci una stringa: ")
-print("Il numero di caratteri senza spazi è: ", count_chars_without_spaces(input_string))
+input_string = input("Enter a string: ")
+print("The number of characters without spaces is: ", count_chars_without_spaces(input_string))
 input("press enter to exit")
